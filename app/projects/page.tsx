ee2bcff1bd487937cb9e9ac1cb3f905416878001
YW <gisakaitatsu@weiyuchens-mbp.lan.uow.edu.au>
2023-08-23 13:49:24 +1000

    add dockerfile

---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'Projects' })
+
+const Grafana = 'This website will be display Grafana and Access Records.'
+export default function Projects() {
+  return Grafana
+}
